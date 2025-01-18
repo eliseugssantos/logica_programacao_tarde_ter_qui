f5bf8fd2a9eaff77c93d222b2be518b8a4725756
eliseugssantos <eliseu.gssantos@senacsp.edu.br>
2025-01-17 23:08:47 -0300

    Adicionar arquivos de projeto.

---
+++ Ex02/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("olá, Mundo! Sou Eliseu");
+Console.WriteLine("Estou aprendendo lógica de programação");
+
---
+++ Ex04/Program.cs
@@ -0,0 +1,16 @@
+﻿//Escrev um programa que pergunte o nome do usuário e apresente nome + boas vindas
+
+Console.WriteLine("Qual é o seu nome?");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Qual é´o seu sobrenome?");
+string sobrenome = Console.ReadLine();
+
+Console.WriteLine("Qual é o seu último sobrenome?");
+string ultimoSobrenome = Console.ReadLine();
+
+//Console.WriteLine("Seja bem-vindo(a) " + nome + sobrenome + ultimoSobrenome );
+Console.WriteLine($"seja bem-vindo(a) {nome} {sobrenome} {ultimoSobrenome} ");
+
+
+Console.WriteLine("Seja Bem-Vindo(a)" + nome);
