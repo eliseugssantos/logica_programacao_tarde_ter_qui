121088f07b322f11e5de3ff729b8b6d29aee9b7d
eliseugssantos <eliseu.gssantos@senacsp.edu.br>
2025-01-18 00:26:02 -0300

    Add files via upload

---
+++ Ex10/Program.cs
@@ -0,0 +1,13 @@
+﻿//Descriçao: Crie um programa que gera a tabuada de um número escolhido pelo usuário.
+//For
+
+
+Console.WriteLine("Digite um número para ver a tabuada");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+for (int contador = 1; contador <= 10; contador++) 
+{
+    Console.WriteLine($"{numero} x{contador} = {numero * contador}");
+}
+
+   
